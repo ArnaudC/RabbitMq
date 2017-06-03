41cdcdf50462a782de469668cfdd4e55e9f6c02a
acouchet <couchet.arnaud@gmail.com>
2017-06-03 21:13:56 +0200

    First commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using System;
+
+namespace Producer {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(">>>> Producer");
+            var connectionFactory = new ConnectionFactory();
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare("hello-world-queue", false, false, false, null);
+            byte[] message = Encoding.UTF8.GetBytes("Producer's message ok!");
+            channel.BasicPublish(string.Empty, "hello-world-queue", null, message);
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Consumer {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(">>>> Customer");
+            var connectionFactory = new ConnectionFactory();
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare("hello-world-queue", false, false, false, null);
+            BasicGetResult result = channel.BasicGet("hello-world-queue", true);
+            if (result != null) {
+                string message = Encoding.UTF8.GetString(result.Body);
+                Console.WriteLine(message);
+            }
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
